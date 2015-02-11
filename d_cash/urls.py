14419a81ed05060a15167c37d64e308c56ef9cc1
vernomcrp <vernomcrp@gmail.com>
2015-02-12 02:13:03 +0700

    Added (partial) image manager

--- d_cash/urls.py
+++ d_cash/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^requestdoc/complete_request_process/$', 'requestdoc.views.complete_request_process'),
     url(r'^requestdoc/request_menu/$', 'requestdoc.views.request_menu'),
     url(r'^requestdoc/request_document_status/$', 'requestdoc.views.list_request_document_status'),
-    url(r'^requestdoc/approve_document/$', 'requestdoc.views.approve_request_document')
+    url(r'^requestdoc/approve_document/$', 'requestdoc.views.approve_request_document'),
+    url(r'^payin/print/(?P<request_doc_id>\d+)/$', 'payin.views.display_payin')
 
 )
--- payin/views.py
+++ payin/views.py
@@ -1,17 +1,29 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from barcode import generate
+
 # Create your views here.
+from requestdoc.models import RequestDoc
+
 
 @login_required
-def display_payin(request):
-    '''
-    Display payin which ready for print
-    :param request:
-    :return:
-    '''
-    context = {}
+def display_payin(request, request_doc_id):
     if request.method == 'GET':
+        user = request.user
+
+        consumer_detail = user.consumerdetail_set.all()[0]
+
+        detail = {
+            'company_name': consumer_detail.consumer_name,
+            'address': consumer_detail.address,
+            'tax_number': consumer_detail.tax_number
+        }
+
+        rdoc = RequestDoc.objects.get(request_user=user, request_doc_number=request_doc_id)
+        filename = generate_barcode(rdoc.request_doc_number, filename='example.svg')
+        return render(request, 'payin/payin_print_ready.html', {'filename': 'example.svg', 'detail': detail})
 
 
-        return render(request, 'payin/payin_print_ready.html', context)
+def generate_barcode(data, filename='example.svg'):
+    return generate('EAN13', data, output=filename)
