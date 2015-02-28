997a78b7e88e7a2a75550e11fa5e157655a4897f
vernomcrp <vernomcrp@gmail.com>
2015-03-01 03:33:15 +0700

    Refactor templates

--- register_new_consumer/views.py
+++ register_new_consumer/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.core.urlresolvers import reverse
 
 from django.contrib.auth.decorators import login_required
-from user_detail.models import ConsumerDetail
+from user_detail.models import ConsumerDetail, UserDetail
 
 
 @login_required
@@ -24,7 +24,7 @@ def registration_page(request):
         address = data.get('address', None)
         tax_number = data.get('tax_number', None)
 
-        user = User.objects.create_user(username, password, email)
+        user = User.objects.create_user(username, email, password)
         user.save()
 
         consumer_detail = ConsumerDetail()
@@ -34,6 +34,14 @@ def registration_page(request):
         consumer_detail.tax_number = tax_number
 
         consumer_detail.save()
+
+        user_detail = UserDetail()
+        user_detail.owner_id = user.id
+        user_detail.role = 'C'
+
+        user_detail.save()
+
+
         request.session['request_user_id'] = user.id
         return HttpResponseRedirect(reverse('requestdoc.views.request_document_view'))
 
