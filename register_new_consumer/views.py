ee91d571d3ebc5e54561663ca5db54390e7d9d78
vernomcrp <vernomcrp@gmail.com>
2015-03-01 16:53:57 +0700

    fix print

--- register_new_consumer/views.py
+++ register_new_consumer/views.py
@@ -13,8 +13,7 @@ logger = logging.getLogger(__name__)
 def registration_page(request):
     if request.method == 'GET':
         # Create user form
-        logger.info(request.user)
-        print request.user
+        print request
         return render(
             request, 'registration/registration_page.html',
             {'logged_in_user': request.user}
